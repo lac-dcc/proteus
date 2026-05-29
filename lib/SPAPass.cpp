09e219c0a6125a2c5393a8e2c15442cd53b08a08
Peter Avgerinos <pa825@ic.ac.uk>
2026-05-29 13:21:43 +0300

    feat(build): Added pre-commit hooks, clang format config and Doxyfile

--- lib/SPAPass.cpp
+++ lib/SPAPass.cpp
@@ -3,11 +3,11 @@
 #include "mlir/Pass/Pass.h"
 
 namespace spa {
-    void SPAPass::runOnOperation() {
-        getOperation().walk([](mlir::Operation* op) {});
-    }
-
-    std::unique_ptr<mlir::Pass> createSPAPass() {
-        return std::make_unique<spa::SPAPass>();
-    }
+void SPAPass::runOnOperation() {
+  getOperation().walk([](mlir::Operation *op) {});
 }
+
+std::unique_ptr<mlir::Pass> createSPAPass() {
+  return std::make_unique<spa::SPAPass>();
+}
+} // namespace spa
