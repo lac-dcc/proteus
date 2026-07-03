fd2ad2617de630926f22f3e1c1edeba7194c21a1
Peter Avgerinos <peteravgerinos@gmail.com>
2026-07-03 13:29:14 +0100

    feat: added vecmat transfer function implementation and tests

--- lib/Analysis/LateralPass.cpp
+++ lib/Analysis/LateralPass.cpp
@@ -49,14 +49,13 @@ proteus::LateralPass::visit(mlir::Operation &op, SparsityEngine &analysis) {
   return mlir::TypeSwitch<mlir::Operation *, std::optional<SparsityLattice>>(
              &op)
       .Case<mlir::linalg::MatmulOp, mlir::linalg::BatchMatmulOp,
-            mlir::linalg::MatvecOp>(
+            mlir::linalg::MatvecOp, mlir::linalg::VecmatOp>(
           [&](auto typedOp) { return visitOp(typedOp, analysis); })
       .Default(
           [&](mlir::Operation *defaultOp) -> std::optional<SparsityLattice> {
             if (defaultOp->getNumResults() == 0) {
               return std::nullopt;
             }
-
             return *analysis.getState(analysis.getCandidateValue());
           });
 }
@@ -133,6 +132,27 @@ proteus::LateralPass::visitOp(mlir::linalg::MatvecOp &op,
   return std::nullopt;
 }
 
+std::optional<proteus::SparsityLattice>
+proteus::LateralPass::visitOp(mlir::linalg::VecmatOp &op,
+                              SparsityEngine &analysis) {
+  auto *lhs = analysis.getState(op.getOperand(0));
+  auto *rhs = analysis.getState(op.getOperand(1));
+
+  if (analysis.getCandidateValue() == op.getOperand(0)) {
+    SparsityLattice candidate = *lhs;
+    candidate[0] &= (*rhs)[0];
+    return candidate;
+  }
+
+  if (analysis.getCandidateValue() == op.getOperand(1)) {
+    SparsityLattice candidate = *rhs;
+    candidate[0] &= (*lhs)[0];
+    return candidate;
+  }
+
+  return std::nullopt;
+}
+
 llvm::SmallVector<mlir::Value>
 proteus::LateralPass::getWorklist(mlir::Block *block) {
   llvm::SmallVector<mlir::Value> worklist;
@@ -140,7 +160,8 @@ proteus::LateralPass::getWorklist(mlir::Block *block) {
   for (auto &op : block->getOperations()) {
     if (mlir::isa<mlir::linalg::MatmulOp>(op) ||
         mlir::isa<mlir::linalg::BatchMatmulOp>(op) ||
-        mlir::isa<mlir::linalg::MatvecOp>(op)) {
+        mlir::isa<mlir::linalg::MatvecOp>(op) ||
+        mlir::isa<mlir::linalg::VecmatOp>(op)) {
       for (auto operand : op.getOperands()) {
         worklist.push_back(operand);
       }
