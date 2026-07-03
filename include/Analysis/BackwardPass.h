d76b8152da3146385a01c9f495026c132d6676d1
Peter Avgerinos <peteravgerinos@gmail.com>
2026-07-03 14:06:39 +0100

    feat: added initial scaffolding of the backward pass

--- include/Analysis/BackwardPass.h
+++ include/Analysis/BackwardPass.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Analysis/SparsityLattice.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/Value.h"
@@ -26,8 +27,20 @@ struct BackwardPass {
    *
    * @param op The operation to visit.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice `op`'s transfer function proposes for
+   * `value`, or std::nullopt if `op` has no lateral transfer function for
+   * that operand.
    */
-  static void visit(mlir::Operation &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visit(mlir::Operation &op, SparsityEngine &analysis);
+
+  /**
+   * @brief Seeds the backward worklist for the worklist fixedpoint algorithm
+   *
+   * @param block The MLIR block to analyse.
+   * @return The lateral worklist in question
+   */
+  static llvm::SmallVector<mlir::Value> getWorklist(mlir::Block *block);
 
 private:
   /**
@@ -35,49 +48,66 @@ private:
    *
    * @param op The matmul op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::MatmulOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::MatmulOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.add operation.
    *
    * @param op The add op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::AddOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::AddOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.matvec operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::MatvecOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::MatvecOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.vecmat operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::VecmatOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::VecmatOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.transpose operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::TransposeOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::TransposeOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.batch_matmul operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::BatchMatmulOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::BatchMatmulOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.fill operation.
@@ -87,32 +117,44 @@ private:
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::FillOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::FillOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.broadcast operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::BroadcastOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::BroadcastOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.pad operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::PadOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::PadOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.concat operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::ConcatOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::ConcatOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.empty operation.
@@ -122,79 +164,99 @@ private:
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::EmptyOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::EmptyOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.conv_2d operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::Conv2DOp &op, SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::Conv2DOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.conv_2d_nchw_fchw operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::Conv2DNchwFchwOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::Conv2DNchwFchwOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a linalg.conv_2d_nhwc_hwcf operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::Conv2DNhwcHwcfOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::Conv2DNhwcHwcfOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for linalg.depthwise_conv_2d_nchw_chw.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::DepthwiseConv2DNchwChwOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::DepthwiseConv2DNchwChwOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for linalg.pooling_nchw_max.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::PoolingNchwMaxOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::PoolingNchwMaxOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for linalg.pooling_nchw_sum.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::linalg::PoolingNchwSumOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::linalg::PoolingNchwSumOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.expand_shape operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::ExpandShapeOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::ExpandShapeOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.extract_slice operation.
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::ExtractSliceOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::ExtractSliceOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for a tensor.collapse_shape operation.
@@ -204,9 +266,11 @@ private:
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitOp(mlir::tensor::CollapseShapeOp &op,
-                      SparsityEngine &analysis);
+  static std::optional<proteus::SparsityLattice>
+  visitOp(mlir::tensor::CollapseShapeOp &op, SparsityEngine &analysis);
 
   /**
    * @brief Infers backward sparsity for ops that pass sparsity through
@@ -215,8 +279,11 @@ private:
    *
    * @param op The op to analyse.
    * @param analysis The SPA analysis object.
+   * @return The candidate lattice for `value`, or std::nullopt if `value` is
+   * not the lhs or rhs operand.
    */
-  static void visitPassthroughOp(mlir::Operation &op, SparsityEngine &analysis);
+  static std::optional<SparsityLattice>
+  visitPassthroughOp(mlir::Operation &op, SparsityEngine &analysis);
 };
 
 } // namespace proteus
--- lib/Analysis/BackwardPass.cpp
+++ lib/Analysis/BackwardPass.cpp
@@ -8,20 +8,48 @@
 #include "llvm/ADT/TypeSwitch.h"
 
 void proteus::BackwardPass::run(mlir::Block *block, SparsityEngine &analysis) {
-  // This time we are going backwards on the block, hence llvm::reverse
-  for (auto &op : llvm::reverse(block->getOperations())) {
-    analysis.visit<BackwardPass>(op);
+  auto worklist = getWorklist(block);
+
+  while (!worklist.empty()) {
+    analysis.setCandidateValue(worklist.pop_back_val());
+
+    auto *current = analysis.getState(analysis.getCandidateValue());
+
+    std::optional<SparsityLattice> joined;
+    for (mlir::Operation *user : analysis.getCandidateValue().getUsers()) {
+      auto candidate = visit(*user, analysis);
+      if (!candidate) {
+        continue;
+      }
+      joined = joined ? SparsityLattice::join(*joined, *candidate) : candidate;
+    }
+
+    if (!joined) {
+      continue;
+    }
+
+    auto merged = SparsityLattice::meet(*current, *joined);
+    if (merged != *current) {
+      *current = merged;
+
+      for (mlir::Operation *user : analysis.getCandidateValue().getUsers()) {
+        for (mlir::Value operand : user->getOperands()) {
+          worklist.push_back(operand);
+        }
+      }
+    }
   }
 }
 
-void proteus::BackwardPass::visit(mlir::Operation &op,
-                                  SparsityEngine &analysis) {
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visit(mlir::Operation &op, SparsityEngine &analysis) {
   if (op.getNumResults() != 1) {
-    return;
+    return *analysis.getState(analysis.getCandidateValue());
   }
 
   // Based on the mlir operation we dispatch the appropriate transfer function
-  mlir::TypeSwitch<mlir::Operation *>(&op)
+  return mlir::TypeSwitch<mlir::Operation *,
+                          std::optional<proteus::SparsityLattice>>(&op)
       .Case<mlir::linalg::MatmulOp, mlir::linalg::AddOp, mlir::linalg::MatvecOp,
             mlir::linalg::VecmatOp, mlir::linalg::TransposeOp,
             mlir::linalg::BatchMatmulOp, mlir::linalg::FillOp,
@@ -32,74 +60,161 @@ void proteus::BackwardPass::visit(mlir::Operation &op,
             mlir::tensor::ConcatOp, mlir::tensor::EmptyOp,
             mlir::tensor::ExpandShapeOp, mlir::tensor::ExtractSliceOp,
             mlir::tensor::CollapseShapeOp>(
-          [&](auto typedOp) -> void { visitOp(typedOp, analysis); })
+          [&](auto typedOp) -> std::optional<proteus::SparsityLattice> {
+            return visitOp(typedOp, analysis);
+          })
       .Case<mlir::linalg::AbsOp, mlir::linalg::CeilOp, mlir::linalg::FloorOp,
             mlir::linalg::NegFOp, mlir::linalg::DivOp,
             mlir::linalg::DivUnsignedOp, mlir::linalg::CopyOp,
             mlir::linalg::TanhOp, mlir::linalg::SquareOp, mlir::linalg::SqrtOp>(
-          [&](auto) -> void { visitPassthroughOp(op, analysis); })
-      .Default([](auto) {});
+          [&](auto) -> std::optional<SparsityLattice> {
+            return visitPassthroughOp(op, analysis);
+          })
+      .Default(
+          [&](mlir::Operation *op) -> std::optional<proteus::SparsityLattice> {
+            if (op->getNumResults() == 0) {
+              return std::nullopt;
+            }
+
+            return *analysis.getState(analysis.getCandidateValue());
+          });
 }
 
-void proteus::BackwardPass::visitOp(mlir::linalg::MatmulOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::MatmulOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::AddOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::AddOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::MatvecOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::MatvecOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::VecmatOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::VecmatOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::TransposeOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::TransposeOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::BatchMatmulOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::BatchMatmulOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::FillOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::FillOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::BroadcastOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::BroadcastOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::PadOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::PadOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::ConcatOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::ConcatOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::EmptyOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::EmptyOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::Conv2DOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::Conv2DOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::Conv2DNchwFchwOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::Conv2DNchwFchwOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::Conv2DNhwcHwcfOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::Conv2DNhwcHwcfOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::DepthwiseConv2DNchwChwOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::DepthwiseConv2DNchwChwOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::PoolingNchwMaxOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::PoolingNchwMaxOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::linalg::PoolingNchwSumOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::linalg::PoolingNchwSumOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::ExpandShapeOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::ExpandShapeOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::ExtractSliceOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::ExtractSliceOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitOp(mlir::tensor::CollapseShapeOp &op,
-                                    SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitOp(mlir::tensor::CollapseShapeOp &op,
+                               SparsityEngine &analysis) {
+  return std::nullopt;
+}
 
-void proteus::BackwardPass::visitPassthroughOp(
-    mlir::Operation &op, SparsityEngine &analysis) { /* TODO */ }
+std::optional<proteus::SparsityLattice>
+proteus::BackwardPass::visitPassthroughOp(mlir::Operation &op,
+                                          SparsityEngine &analysis) {
+  return std::nullopt;
+}
+
+llvm::SmallVector<mlir::Value>
+proteus::BackwardPass::getWorklist(mlir::Block *block) {
+  llvm::SmallVector<mlir::Value> worklist;
+
+  for (auto &op : block->getOperations()) {
+    for (auto operand : op.getOperands()) {
+      worklist.push_back(operand);
+    }
+  }
+
+  return worklist;
+}
