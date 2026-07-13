d8f77acfc7b5f12fefb91f2d0d7c1e8a53b2790b
Peter Avgerinos <peteravgerinos@gmail.com>
2026-07-13 13:59:57 +0300

    feat: added external dependency of mlir-probe and added dialect and transform into proteus-opt

--- tools/proteus-opt/proteus-opt.cpp
+++ tools/proteus-opt/proteus-opt.cpp
@@ -1,5 +1,7 @@
 #include "Analysis/SparsityPropagationAnalysis.h"
 
+#include "Dialect/Probe/IR/Probe.h"
+#include "Dialect/Probe/Transforms/Passes.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
@@ -9,9 +11,11 @@
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
     registry.insert<mlir::arith::ArithDialect, mlir::func::FuncDialect,
-                    mlir::linalg::LinalgDialect, mlir::tensor::TensorDialect>();
+                    mlir::linalg::LinalgDialect, mlir::tensor::TensorDialect,
+                    mlir::probe::ProbeDialect>();
 
     mlir::PassRegistration<proteus::SparsityPropagationAnalysis>();
+    mlir::probe::registerProbePasses();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Tensor Slice Sparsity Propagation Pass\n", registry));
